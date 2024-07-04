84ecb21ac5a2e88e7c6879b8cda410027dc7e468
lianghanzhong <wyattliang@gmail.com>
2024-07-04 17:16:15 +0800

    Fix typo

--- lib/icon.dart
+++ lib/icon.dart
@@ -28,14 +28,7 @@ class InnoSetupIconsBuilder {
   @override
   String toString() => '''
 [Icons]
-Name: "{autoprograms}\\${_app.name}"; Filename: "{app}\\${_app.executable}"
-Name: "{autodesktop}\\${_app.name}"; Filename: "{app}\\${_app.executable}; Tasks: desktopicon"
+Name: "{autoprograms}\\${_app.name}"; Filename: "{app}\\${_app.name}"
+Name: "{autodesktop}\\${_app.name}"; Filename: "{app}\\${_app.name}"; Tasks: desktopicon
 ''';
-
-//   @override
-//   String toString() => '''
-// [Icons]
-// Name: "{autoprograms}\\${_app.name}"; Filename: "{app}\\${_app.name}"
-// Name: "{autodesktop}\\${_app.name}"; Filename: "{app}\\${_app.name}"; Tasks: desktopicon
-// ''';
 }
